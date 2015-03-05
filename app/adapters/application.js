af5fe59ce61e3f868b29d52f0f0d44a751835703
Damien Bachet <dbachet@gmail.com>
2015-03-05 13:54:09 +0100

    Oh la boulette! ;)

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,5 @@ import DS from "ember-data";
 
 export default DS.ActiveModelAdapter.extend({
   namespace: "api/v1",
-  host: "http://production.changeons-frontend.divshot.io/"
+  host: "changeons-prod.herokuapp.com"
 });
