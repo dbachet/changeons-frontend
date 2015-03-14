b21164ff91e368ed86fa759fb1b08fc017f79e16
Damien Bachet <dbachet@gmail.com>
2015-03-14 20:45:52 +0100

    Put twitter share button into a component

---
+++ app/components/share-twitter.js
@@ -0,0 +1,7 @@
+import Ember from "ember";
+
+export default Ember.TextField.extend({
+  tagName: "a",
+  classNames: "twitter-share-button",
+  attributeBindings: ["data-size", "data-url", "data-text", "data-hashtags", "data-count"]
+});
