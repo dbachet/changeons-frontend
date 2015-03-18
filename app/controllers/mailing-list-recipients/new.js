8ca615f14dc78325f77621c030e25f6374bf45e5
Damien Bachet <dbachet@gmail.com>
2015-03-18 08:25:31 +0100

    Better social share message + confidentiality label

--- app/controllers/mailing-list-recipients/new.js
+++ app/controllers/mailing-list-recipients/new.js
@@ -14,8 +14,7 @@ export default Ember.ObjectController.extend({
   email: null,
 
   whenHasSucceeded: function() {
-    Ember.$(".inputs").hide();
-    Ember.$(".notify-me").hide();
+    Ember.$("#mailing-form").hide();
 
     Ember.$("section#logo").animate({ padding: "20px 0" }, 500);
     if (Ember.$("body").width() >= 992) {
@@ -26,16 +25,17 @@ export default Ember.ObjectController.extend({
 
     Ember.$("#main-message div").html("").css({ opacity: 0 });
 
-    Ember.$("#main-message").prepend("<div class='col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-4 success-message alert alert-success' role='alert'>Vous allez recevoir un email pour confirmer votre inscription à la liste d'attente.</div>");
-    Ember.$("#main-message p.alert-success").css({ opacity: 0 });
+    Ember.$("#main-message").prepend("<div class='col-xs-10 col-xs-offset-1 col-md-8 col-md-offset-2 success-message alert alert-success' role='alert'>Vous allez recevoir un email pour confirmer votre inscription à la liste d'attente.</br></br>Remarque : si vous ne recevez pas le mail de confirmation juste après votre inscription, veuillez vérifier dans votre boîte SPAM ou essayer de vous réinscrire avec une autre adresse email.</div>");
+
+    Ember.$("#main-message div.alert-success").css({ opacity: 0 });
 
     Ember.$("#social-share").show();
     Ember.$("#social-share div").css({ opacity: 0 });
 
     Ember.$("body").animate({scrollTop : 0}, 300)
 
-    Ember.$("#main-message p.alert-success").delay(500).animate({ opacity: 1 }, 500);
-    Ember.$("#main-message div, #social-share div").delay(1000).animate({ opacity: 1 }, 500);
+    Ember.$("#main-message div").delay(1000).animate({ opacity: 1 }, 500);
+    Ember.$("#social-share div").delay(2000).animate({ opacity: 1 }, 500);
 
   }.observes("hasSucceeded"),
 
