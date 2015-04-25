db8958cd8042149cdedcd2b61834a79e31f271c8
Damien Bachet <dbachet@gmail.com>
2015-04-25 22:54:21 +0200

    Add category query param on links controller

--- app/controllers/links.js
+++ app/controllers/links.js
@@ -3,5 +3,18 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   init: function(){
     moment.locale('fr');
-  }
+  },
+  queryParams: ['category'],
+  category: null,
+
+  filteredArticles: function() {
+    var category = this.get('category');
+    var links = this.get('model');
+
+    if (category) {
+      return links.filterBy('category', category);
+    } else {
+      return links;
+    }
+  }.property('category', 'model')
 });
