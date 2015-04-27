18c8582e80354a3cd7c98bacf036c053e8b78c6c
Damien Bachet <dbachet@gmail.com>
2015-04-27 22:26:20 +0200

    move category menu to application

--- app/controllers/links.js
+++ app/controllers/links.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   init: function(){
     moment.locale('fr');
-    this.send("initToCloseCategoryMenuOnOutsideClick");
   },
   queryParams: ['category'],
   category: null,
@@ -17,23 +16,5 @@ export default Ember.Controller.extend({
     } else {
       return links;
     }
-  }.property('category', 'model'),
-
-  categoryMenuExpanded: false,
-
-  actions: {
-    toggleCategoryMenu: function() {
-      if (!this.get("categoryMenuExpanded")){
-        this.set("categoryMenuExpanded", true);
-      }
-    },
-    initToCloseCategoryMenuOnOutsideClick: function() {
-      var self = this;
-      Ember.$("html").click(function(e) {
-        if(!Ember.$(event.target).is('.category-menu .not-expanded a, .category-menu .not-expanded span')){
-          self.set("categoryMenuExpanded", false);
-        }
-      });
-    }
-  }
+  }.property('category', 'model')
 });
--- app/views/application.js
+++ app/views/application.js
@@ -1,3 +1,10 @@
 import Ember from "ember";
 
-export default Ember.View.extend({});
+export default Ember.View.extend({
+  click: function() {
+    // Close the category menu on click if expanded
+    if(!Ember.$(event.target).is('.category-menu .not-expanded a, .category-menu .not-expanded span')){
+      this.set("controller.categoryMenuIsExpanded", false);
+    }
+  },
+});
