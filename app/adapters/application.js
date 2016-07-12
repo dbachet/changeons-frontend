9f234679aee9eeff44c1ab4e139c8a09fca65b57
Damien Bachet <dbachet@gmail.com>
2016-07-12 13:33:06 +0200

    update broccoli-sass and remove temporarily content-security-policy

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,5 @@ import DS from "ember-data";
 
 export default DS.ActiveModelAdapter.extend({
   namespace: "api/v1",
-  host: "https://changeons-prod.herokuapp.com/"
+  host: "https://changeons-prod.herokuapp.com"
 });
