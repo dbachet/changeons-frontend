9b38cb1541ca8558099eebaffd415d14fddc353b
Damien Bachet <dbachet@gmail.com>
2016-07-19 10:48:25 +0200

    Update ember cli 2.7.0-beta.6

--- app/router.js
+++ app/router.js
@@ -1,13 +1,16 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-var Router = Ember.Router.extend({
-  location: config.locationType
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
-export default Router.map(function() {
-  this.resource("shares", {path: "/"});
+Router.map(function() {
+  this.route("shares", { path: "/"});
   this.route("about");
   this.route("contact");
   this.route("credits");
 });
+
+export default Router;
